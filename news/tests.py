aba47e826d0532d79b09bd046e3fa292fe7063c5
bwhicks <benjamin.w.hicks@gmail.com>
2017-07-21 12:02:26 -0400

    Add post migration and tests

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+from .models import Post
+
+
+class TestPost(TestCase):
+
+    def test_str(self):
+        post = Post.objects.create(subject='foo', content='bar')
+        assert isinstance(post, Post)
+        print(str(post))
+        assert str(post) == "%s - foo" % timezone.now().date()
--- plinyproj/settings.py
+++ plinyproj/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     # local apps
     'letters',
     'prosopography',
+    'news',
 ]
 
 MIDDLEWARE = [
@@ -113,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
