f37faaddae2e313299bd22cab0240cec14c72883
bwhicks <benjamin.w.hicks@gmail.com>
2017-08-10 14:28:14 -0400

    Fix tests, reset conditional get

--- news/tests.py
+++ news/tests.py
@@ -11,8 +11,8 @@ class TestPost(TestCase):
     def test_str(self):
         post = Post.objects.create(subject='foo', content='bar')
         assert isinstance(post, Post)
-        print(str(post))
-        assert str(post) == "%s - foo" % timezone.now().date()
+        assert str(timezone.now().date()) in str(post)
+        assert " - foo" in str(post)
 
 
 class TestPostListView(TestCase):
