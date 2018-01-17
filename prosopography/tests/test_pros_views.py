717380564ebfe7a4443aad64fa21ba4c68b3157e
Benjamin Hicks <benjamin.w.hicks@gmail.com>
2018-01-17 10:45:44 -0500

    Add tests for dal autocomplete also

--- prosopography/tests/test_pros_views.py
+++ prosopography/tests/test_pros_views.py
@@ -1,10 +1,49 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
 from prosopography.models import Person
 
 
-class TestPersonAutocomplete(TestCase):
+class PersonAutoCompleteDAL(TestCase):
+
+    def setUp(self):
+
+        self.quintus = Person.objects.create(nomina='Quintus')
+        self.gaius = Person.objects.create(nomina='Gaius')
+
+        self.user = get_user_model().objects.create_user(
+            username='foo',
+            password='bar'
+        )
+        self.user.is_staff = True
+        self.user.save()
+
+    def test_get(self):
+        auto = reverse('people:dal-autocomplete')
+        # no query string, no login, 302
+        res = self.client.get(auto)
+        assert res.status_code == 302
+
+        # logged in, should get both created people
+        self.client.login(username='foo', password='bar')
+        res = self.client.get(auto)
+        data = res.json()
+        assert data
+        assert 'results' in data
+        assert len(data['results']) == 2
+
+        # search for Qu
+        res = self.client.get(auto, {'q': 'Qu'})
+        assert res.status_code == 200
+        data = res.json()
+        assert len(data['results']) == 1
+        # should get back Quintus' nomina
+        print(data['results'])
+        assert data['results'][0]['text'] == 'Quintus'
+
+
+class PersonAutocomplete(TestCase):
 
     def setUp(self):
 
@@ -13,7 +52,7 @@ class TestPersonAutocomplete(TestCase):
 
     def test_get(self):
         auto = reverse('people:autocomplete')
-        # no query string, no result
+        # no query string
         res = self.client.get(auto)
         assert res.status_code == 200
         data = res.json()
--- prosopography/urls.py
+++ prosopography/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
-from .views import (PersonListView, person_autocomplete, NodeEdgeListView)
+from .views import (PersonListView, person_autocomplete,
+                    PersonAutoComplete, NodeEdgeListView)
 
 urlpatterns = [
     url(r'^$', PersonListView.as_view(), name='search'),
     url(r'^autocomplete/$', person_autocomplete, name='autocomplete'),
+    url(r'^dal-autocomplete/$', PersonAutoComplete.as_view(),
+        name='dal-autocomplete'),
     url(r'^nodes.json$', NodeEdgeListView.as_view(), name='nodes'),
 
 ]
