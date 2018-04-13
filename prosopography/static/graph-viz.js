485237dc37469410d8de768083288e463202254b
Benjamin Hicks <benjamin.w.hicks@gmail.com>
2018-04-13 16:04:31 -0400

    Update styles and templates for person detail/viz

--- prosopography/static/graph-viz.js
+++ prosopography/static/graph-viz.js
@@ -5,7 +5,7 @@ var svg = d3.select("svg"),
     width = +svg.attr("width"),
     height = +svg.attr("height");
 
-var simulation = d3.forceSimulation()
+simulation = d3.forceSimulation()
     .force("link", d3.forceLink().id(function(d) { return d.id; }))
     .force("charge", d3.forceManyBody().strength(-300))
     .force("center", d3.forceCenter(width / 3, height / 3));
